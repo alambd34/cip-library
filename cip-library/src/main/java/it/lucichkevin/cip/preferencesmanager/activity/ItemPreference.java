72b6f3c9bea90a4ff5a616c1138e1e83f9f23024
Kevin <lucichkevin@gmail.com>
2018-08-24 18:52:21 +0200

    Release v2.4.1

--- cip-library/src/main/java/it/lucichkevin/cip/preferencesmanager/activity/ItemPreference.java
+++ cip-library/src/main/java/it/lucichkevin/cip/preferencesmanager/activity/ItemPreference.java
@@ -12,6 +12,9 @@ import java.util.ArrayList;
  *  @author	Kevin Lucich 11/09/2014
  *  @since	CipLibrary v0.5.0
  *  @update	2018-08-08
+ *
+ *  2018-08-24
+ *  	Fix for ListPreferenc: add PreferenceListEntry to keep value and label and keep the order entries list
  */
 public class ItemPreference {
 
