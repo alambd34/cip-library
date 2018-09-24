52d8693685ceefb732270fefc91b075f41f24aa3
Kevin <lucichkevin@gmail.com>
2018-09-24 15:18:40 +0200

    Release v2.7.2 - Suppress deprecation warnings

--- cip-library/src/main/java/it/lucichkevin/cip/dialogs/pickers/TimePickerDialog.java
+++ cip-library/src/main/java/it/lucichkevin/cip/dialogs/pickers/TimePickerDialog.java
@@ -18,188 +18,191 @@ import java.util.Calendar;
 import it.lucichkevin.cip.*;
 
 /**
-    Create a dialog with a two NumberPickers to choose hours and minutes
+	Create a dialog with a two NumberPickers to choose hours and minutes
 
-    @author     Kevin Lucich    2014-09-03
-    @since      0.3.0
+	@author	 Kevin Lucich	2014-09-03
+	@since	  0.3.0
 */
 public class TimePickerDialog extends DialogFragment {
 
-    protected final static int BUTTON_POSITIVE = R.id.btn_positive;
-    protected final static int BUTTON_NEGATIVE = R.id.btn_negative;
+	protected final static int BUTTON_POSITIVE = R.id.btn_positive;
+	protected final static int BUTTON_NEGATIVE = R.id.btn_negative;
 
-    protected Dialog timePickerDialog = null;
-    protected TimePicker timePicker = null;
-    protected Holder viewHolder = null;
+	protected Dialog timePickerDialog = null;
+	protected TimePicker timePicker = null;
+	protected Holder viewHolder = null;
 
-    //  Default behavior - Log (if in debug) and close dialog
-    protected Callbacks callbacks = new EmptyCallbacks(){
-        public void onButtonPositiveClicked( Dialog dialog, int hour, int minute ){
-            super.onButtonPositiveClicked( dialog, hour, minute );
-//            Utils.logger("Cip.TimePickerDialog", "Callback onButtonPositiveClicked( Button, "+ String.valueOf(hour) +", "+ String.valueOf(minute) +" ) called!", Utils.LOG_INFO);
-        }
-        public void onButtonCancelClicked( Dialog dialog, int hour, int minute ){
-            super.onButtonCancelClicked( dialog, hour, minute );
-//            Utils.logger("Cip.TimePickerDialog", "Callback onButtonCancelClicked( Button, "+ String.valueOf(hour) +", "+ String.valueOf(minute) +" ) called!", Utils.LOG_INFO);
-        }
-    };
+	//  Default behavior - Log (if in debug) and close dialog
+	protected Callbacks callbacks = new EmptyCallbacks(){
+		public void onButtonPositiveClicked( Dialog dialog, int hour, int minute ){
+			super.onButtonPositiveClicked( dialog, hour, minute );
+//			Utils.logger("Cip.TimePickerDialog", "Callback onButtonPositiveClicked( Button, "+ String.valueOf(hour) +", "+ String.valueOf(minute) +" ) called!", Utils.LOG_INFO);
+		}
+		public void onButtonCancelClicked( Dialog dialog, int hour, int minute ){
+			super.onButtonCancelClicked( dialog, hour, minute );
+//			Utils.logger("Cip.TimePickerDialog", "Callback onButtonCancelClicked( Button, "+ String.valueOf(hour) +", "+ String.valueOf(minute) +" ) called!", Utils.LOG_INFO);
+		}
+	};
 
-    public TimePickerDialog(){
-        this(Utils.getContext());
-    }
+	public TimePickerDialog(){
+		this(Utils.getContext());
+	}
 
-    public TimePickerDialog( Context context ){
+	@SuppressWarnings("deprecation")
+	public TimePickerDialog( Context context ){
 
-        timePickerDialog = new Dialog(context);
-        timePickerDialog.setContentView(R.layout.timepicker_layout);
-        timePickerDialog.setTitle("TEST");
-        timePickerDialog.setCancelable(false);
+		timePickerDialog = new Dialog(context);
+		timePickerDialog.setContentView(R.layout.timepicker_layout);
+		timePickerDialog.setTitle("TEST");
+		timePickerDialog.setCancelable(false);
 
-        timePicker = ((TimePicker) timePickerDialog.findViewById(R.id.timepicker));
+		timePicker = ((TimePicker) timePickerDialog.findViewById(R.id.timepicker));
 
-        final Calendar c = Calendar.getInstance();
+		final Calendar c = Calendar.getInstance();
 
-        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ){
-            timePicker.setHour(c.get(Calendar.HOUR_OF_DAY));
-            timePicker.setMinute(c.get(Calendar.MINUTE));
-        }else{
-            timePicker.setCurrentHour(c.get(Calendar.HOUR_OF_DAY));
-            timePicker.setCurrentMinute(c.get(Calendar.MINUTE));
-        }
+		if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ){
+			timePicker.setHour(c.get(Calendar.HOUR_OF_DAY));
+			timePicker.setMinute(c.get(Calendar.MINUTE));
+		}else{
+			timePicker.setCurrentHour(c.get(Calendar.HOUR_OF_DAY));
+			timePicker.setCurrentMinute(c.get(Calendar.MINUTE));
+		}
 
-        timePicker.setIs24HourView(DateFormat.is24HourFormat(context));
+		timePicker.setIs24HourView(DateFormat.is24HourFormat(context));
 
-        viewHolder = new Holder(timePicker);
-    }
+		viewHolder = new Holder(timePicker);
+	}
 
-    @Override
-    public Dialog onCreateDialog( Bundle savedInstanceState ){
+	@Override
+	@SuppressWarnings("deprecation")
+	public Dialog onCreateDialog( Bundle savedInstanceState ){
 
-        try {
-            (viewHolder.hour).setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-                @Override
-                public void onValueChange( NumberPicker picker, int oldVal, int newVal ){
-                    timePicker.setCurrentHour(newVal);
-                }
-            });
+		try {
+			(viewHolder.hour).setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+				@Override
+				public void onValueChange( NumberPicker picker, int oldVal, int newVal ){
+					timePicker.setCurrentHour(newVal);
+				}
+			});
 
-            (viewHolder.minute).setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-                @Override
-                public void onValueChange( NumberPicker picker, int oldVal, int newVal ){
-                    timePicker.setCurrentMinute( newVal );
-                }
-            });
+			(viewHolder.minute).setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+				@Override
+				public void onValueChange( NumberPicker picker, int oldVal, int newVal ){
+					timePicker.setCurrentMinute( newVal );
+				}
+			});
 
-        }catch( Exception e ){
-            e.printStackTrace();
-        }
+		}catch( Exception e ){
+			e.printStackTrace();
+		}
 
-        setCallbacksOfButtons();
+		setCallbacksOfButtons();
 
-        return timePickerDialog;
-    }
+		return timePickerDialog;
+	}
 
-    protected void setCallbacksOfButtons() {
+	@SuppressWarnings("deprecation")
+	protected void setCallbacksOfButtons() {
 
-        ((Button) timePickerDialog.findViewById(BUTTON_POSITIVE)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick( View view ){
-                if( viewHolder.minute != null ){
-                    viewHolder.minute.clearFocus();
-                }
-                if( viewHolder.hour != null ){
-                    viewHolder.hour.clearFocus();
-                }
-                callbacks.onButtonPositiveClicked( timePickerDialog, timePicker.getCurrentHour(), timePicker.getCurrentMinute() );
-            }
-        });
-        ((Button) timePickerDialog.findViewById(BUTTON_NEGATIVE)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick( View view ){
-                callbacks.onButtonCancelClicked( timePickerDialog, timePicker.getCurrentHour(), timePicker.getCurrentMinute() );
-            }
-        });
-    }
+		((Button) timePickerDialog.findViewById(BUTTON_POSITIVE)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick( View view ){
+				if( viewHolder.minute != null ){
+					viewHolder.minute.clearFocus();
+				}
+				if( viewHolder.hour != null ){
+					viewHolder.hour.clearFocus();
+				}
+				callbacks.onButtonPositiveClicked( timePickerDialog, timePicker.getCurrentHour(), timePicker.getCurrentMinute() );
+			}
+		});
+		((Button) timePickerDialog.findViewById(BUTTON_NEGATIVE)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick( View view ){
+				callbacks.onButtonCancelClicked( timePickerDialog, timePicker.getCurrentHour(), timePicker.getCurrentMinute() );
+			}
+		});
+	}
 
-    public void show( FragmentManager fragmentManager ){
-        super.show( fragmentManager, "PickerDialog-" + Math.random()*100 );
-    }
+	public void show( FragmentManager fragmentManager ){
+		super.show( fragmentManager, "PickerDialog-" + Math.random()*100 );
+	}
 
-    ///////////////////////////////
-    //  Getters and Setters
+	///////////////////////////////
+	//  Getters and Setters
 
-    public int getHour(){
-        return timePicker.getCurrentHour();
-    }
-    public void setHour( int hour ){
-        timePicker.setCurrentHour(hour);
-    }
+	public int getHour(){
+		return timePicker.getCurrentHour();
+	}
+	public void setHour( int hour ){
+		timePicker.setCurrentHour(hour);
+	}
 
-    public int getMinute(){
-        return timePicker.getCurrentMinute();
-    }
-    public void setMinute( int minute ){
-        timePicker.setCurrentMinute(minute);
-    }
+	public int getMinute(){
+		return timePicker.getCurrentMinute();
+	}
+	public void setMinute( int minute ){
+		timePicker.setCurrentMinute(minute);
+	}
 
-    public void setCallbacks( Callbacks callbacks ){
-        this.callbacks = callbacks;
-    }
+	public void setCallbacks( Callbacks callbacks ){
+		this.callbacks = callbacks;
+	}
 
-    public void setIs24HourFormat( boolean is24HourFormat ){
-        timePicker.setIs24HourView(is24HourFormat);
-    }
+	public void setIs24HourFormat( boolean is24HourFormat ){
+		timePicker.setIs24HourView(is24HourFormat);
+	}
 
-    public Dialog getDialog(){
-        return timePickerDialog;
-    }
-
-
-
-
-    /////////////////////////////////////
-    //  Callbacks
-
-    public static interface Callbacks {
-        public void onButtonPositiveClicked( Dialog dialog, int hour, int minute);
-        public void onButtonCancelClicked( Dialog dialog, int hour, int minute);
-    }
-
-    public static class EmptyCallbacks implements Callbacks {
-        @Override
-        public void onButtonPositiveClicked( Dialog dialog, int hour, int minute ){
-            dialog.dismiss();
-        }
-        @Override
-        public void onButtonCancelClicked( Dialog dialog, int hour, int minute ){
-            dialog.dismiss();
-        }
-    }
+	public Dialog getDialog(){
+		return timePickerDialog;
+	}
 
 
 
-    /**
-     *  Used to hold the elements of TimePicker
-     */
-    protected class Holder {
 
-        public NumberPicker hour = null;
-        public NumberPicker minute = null;
-        public TextView divider = null;
+	/////////////////////////////////////
+	//  Callbacks
 
-        public Holder( TimePicker timePicker ){
+	public static interface Callbacks {
+		public void onButtonPositiveClicked( Dialog dialog, int hour, int minute);
+		public void onButtonCancelClicked( Dialog dialog, int hour, int minute);
+	}
 
-            try {
-                Class<?> classForId = Class.forName("com.android.internal.R$id");
+	public static class EmptyCallbacks implements Callbacks {
+		@Override
+		public void onButtonPositiveClicked( Dialog dialog, int hour, int minute ){
+			dialog.dismiss();
+		}
+		@Override
+		public void onButtonCancelClicked( Dialog dialog, int hour, int minute ){
+			dialog.dismiss();
+		}
+	}
 
-                this.hour = (NumberPicker) timePicker.findViewById( (classForId.getField("hour")).getInt(null) );
-                this.minute = (NumberPicker) timePicker.findViewById( (classForId.getField("minute")).getInt(null) );
-                this.divider = (TextView) timePicker.findViewById( (classForId.getField("divider")).getInt(null) );
 
-            }catch( Exception e ){
-                e.printStackTrace();
-            }
-        }
-    }
+
+	/**
+	 *  Used to hold the elements of TimePicker
+	 */
+	protected class Holder {
+
+		public NumberPicker hour = null;
+		public NumberPicker minute = null;
+		public TextView divider = null;
+
+		public Holder( TimePicker timePicker ){
+
+			try {
+				Class<?> classForId = Class.forName("com.android.internal.R$id");
+
+				this.hour = (NumberPicker) timePicker.findViewById( (classForId.getField("hour")).getInt(null) );
+				this.minute = (NumberPicker) timePicker.findViewById( (classForId.getField("minute")).getInt(null) );
+				this.divider = (TextView) timePicker.findViewById( (classForId.getField("divider")).getInt(null) );
+
+			}catch( Exception e ){
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
